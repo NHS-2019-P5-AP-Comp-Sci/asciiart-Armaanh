e2f5c2a38c7365048a40497cbea50106d1b1c63e
s-hudaar <s-hudaar@53S500036704.it.bsd405.org>
2019-09-20 11:54:25 -0700

    turning project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,27 +1,144 @@
 /**
  * Unit 1 Project
  * 
- * In this project you have the opportunity to create a piece of artwork using ASCII 
- * (American Standard Code for Information Interchange). Using the already created file 
- * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
- * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
- * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
+ * In this project you have the opportunity to create a piece of artwork using
+ * ASCII (American Standard Code for Information Interchange). Using the already
+ * created file named `ASCIIArt.java` write a program that outputs your artwork
+ * to the console. The goal of this project is to use ONLY what we have learned
+ * in Unit 1 to create anything you would like so be creative (and school
+ * appropriate). Artwork must also have a caption describing what it's
+ * depicting.
  * 
- * For those of you who have dabbled in coding before, try to contain your code to 
- * just what we have discussed in this unit.  
+ * For those of you who have dabbled in coding before, try to contain your code
+ * to just what we have discussed in this unit.
  * 
- * @author Mr. Rasmussen 
+ * @author Mr. Rasmussen
  *
  */
 
 // YOU CODE HERE :) 
 
 public class ASCIIArt {
-	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+	public static void main(String[] args) {
+		// CODE HERE
+		smiley();
+		frowny();
+		whoa();
+		text();
+	}
+
+	public static void smiley() {
+		System.out.println("                              @@@@@@@@@@@@@@@@@@                            ");
+		System.out.println("                          @@@@@@@@@@@@@@@@@@@@@@@@@@                        ");
+		System.out.println("                       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                     ");
+		System.out.println("                    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                  ");
+		System.out.println("                  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                ");
+		System.out.println("                @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@               ");
+		System.out.println("              @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            ");
+		System.out.println("             @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@           ");
+		System.out.println("            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@          ");
+		System.out.println("           @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@         ");
+		System.out.println("          @@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@        ");
+		System.out.println("         @@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@       ");
+		System.out.println("        @@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@@      ");
+		System.out.println("       @@@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@@@     ");
+		System.out.println("       @@@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@@@@    ");
+		System.out.println("      @@@@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@@@@@   ");
+		System.out.println("      @@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@   ");
+		System.out.println("      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   ");
+		System.out.println("       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   ");
+		System.out.println("       @@@@@@@@@@@@     |   | |    | |  \\     /\\           @@@@@@@@@@@@@@   ");
+		System.out.println("       @@@@@@@@@@@@@    |___| |    | |   \\   /  \\         @@@@@@@@@@@@@@    ");
+		System.out.println("        @@@@@@@@@@@@@   |   | |    | |   /  /____\\       @@@@@@@@@@@@@@     ");
+		System.out.println("         @@@@@@@@@@@@@  |   | \\____/ |__/  /      \\    @@@@@@@@@@@@@@@      ");
+		System.out.println("          @@@@@@@@@@@@@@                             @@@@@@@@@@@@@@@@        ");
+		System.out.println("           @@@@@@@@@@@@@@@                          @@@@@@@@@@@@@@@@        ");
+		System.out.println("            @@@@@@@@@@@@@@@@                      @@@@@@@@@@@@@@@@@         ");
+		System.out.println("              @@@@@@@@@@@@@@@@                  @@@@@@@@@@@@@@@@@           ");
+		System.out.println("                 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@              ");
+		System.out.println("                    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                 ");
+		System.out.println("                        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                     ");
+		System.out.println("                            @@@@@@@@@@@@@@@@@@@@@@@                         ");
+		System.out.println("                               @@@@@@@@@@@@@@@@@                            ");
+		System.out.println(" \n SHAWTY SAYS YEAAAAAAAAAAAAA HUDA MANNNNNN!!!!!!!!!!!!!!!");
+	}
+
+	public static void text() {
+		System.out.println("\nTHIS IS MY ASCII ART!!!!");
+		System.out.println("\nTHIS IS MY ASCII ART!!!!");
+		System.out.println("\nTHIS IS MY ASCII ART!!!!");
+		System.out.println("\nTHIS IS MY ASCII ART!!!!");
+		System.out.println("\nTHIS IS MY ASCII ART!!!!");
+		System.out.println("\nYou're welcome");
+	}
+	public static void frowny() {
+		System.out.println("                              @@@@@@@@@@@@@@@@@@                            ");
+		System.out.println("                          @@@@@@@@@@@@@@@@@@@@@@@@@@                        ");
+		System.out.println("                       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                     ");
+		System.out.println("                    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                  ");
+		System.out.println("                  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                ");
+		System.out.println("                @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@               ");
+		System.out.println("              @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            ");
+		System.out.println("             @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@           ");
+		System.out.println("            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@          ");
+		System.out.println("           @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@         ");
+		System.out.println("          @@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@        ");
+		System.out.println("         @@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@       ");
+		System.out.println("        @@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@@      ");
+		System.out.println("       @@@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@@@     ");
+		System.out.println("       @@@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@@@@    ");
+		System.out.println("      @@@@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@@@@@   ");
+		System.out.println("      @@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@   ");
+		System.out.println("      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   ");
+		System.out.println("       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   ");
+		System.out.println("       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   ");
+		System.out.println("       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    ");
+		System.out.println("        @@@@@@@@@@@@@@@@@@@@@@@@@@@@        @@@@@@@@@@@@@@@@@@@@@@@@@@@     ");
+		System.out.println("         @@@@@@@@@@@@@@@@@@@@@@@@              @@@@@@@@@@@@@@@@@@@@@@@      ");
+		System.out.println("          @@@@@@@@@@@@@@@@@@@@@                  @@@@@@@@@@@@@@@@@@@        ");
+		System.out.println("           @@@@@@@@@@@@@@@@@@                      @@@@@@@@@@@@@@@@@        ");
+		System.out.println("            @@@@@@@@@@@@@@                           @@@@@@@@@@@@@@         ");
+		System.out.println("              @@@@@@@@@@@                             @@@@@@@@@@@           ");
+		System.out.println("                 @@@@@@@                               @@@@@@@              ");
+		System.out.println("                    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                 ");
+		System.out.println("                        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                     ");
+		System.out.println("                            @@@@@@@@@@@@@@@@@@@@@@@                         ");
+		System.out.println("                               @@@@@@@@@@@@@@@@@                            ");
+		System.out.println(" \n SHAWTY SAYS GOSH DARN IT, NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!!!!");
+	}
+	public static void whoa() {
+		System.out.println("                              @@@@@@@@@@@@@@@@@@                            ");
+		System.out.println("                          @@@@@@@@@@@@@@@@@@@@@@@@@@                        ");
+		System.out.println("                       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                     ");
+		System.out.println("                    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                  ");
+		System.out.println("                  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                ");
+		System.out.println("                @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@               ");
+		System.out.println("              @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            ");
+		System.out.println("             @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@           ");
+		System.out.println("            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@          ");
+		System.out.println("           @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@         ");
+		System.out.println("          @@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@        ");
+		System.out.println("         @@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@       ");
+		System.out.println("        @@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@@      ");
+		System.out.println("       @@@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@@@     ");
+		System.out.println("       @@@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@@@@    ");
+		System.out.println("      @@@@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@@@@@   ");
+		System.out.println("      @@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@   ");
+		System.out.println("      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   ");
+		System.out.println("       @@@@@@@@@@@@@@@@@@@@@@@@@@         @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   ");
+		System.out.println("       @@@@@@@@@@@@@@@@@@@@@@@@             @@@@@@@@@@@@@@@@@@@@@@@@@@@@@   ");
+		System.out.println("       @@@@@@@@@@@@@@@@@@@@@@                 @@@@@@@@@@@@@@@@@@@@@@@@@@    ");
+		System.out.println("        @@@@@@@@@@@@@@@@@@@                     @@@@@@@@@@@@@@@@@@@@@@@     ");
+		System.out.println("         @@@@@@@@@@@@@@@@                         @@@@@@@@@@@@@@@@@@@@      ");
+		System.out.println("          @@@@@@@@@@@@@@@@@                     @@@@@@@@@@@@@@@@@@@@        ");
+		System.out.println("           @@@@@@@@@@@@@@@@@@                 @@@@@@@@@@@@@@@@@@@@@@        ");
+		System.out.println("            @@@@@@@@@@@@@@@@@@@             @@@@@@@@@@@@@@@@@@@@@@@         ");
+		System.out.println("              @@@@@@@@@@@@@@@@@@@         @@@@@@@@@@@@@@@@@@@@@@@           ");
+		System.out.println("                 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@              ");
+		System.out.println("                    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                 ");
+		System.out.println("                        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                     ");
+		System.out.println("                            @@@@@@@@@@@@@@@@@@@@@@@                         ");
+		System.out.println("                               @@@@@@@@@@@@@@@@@                            ");
+		System.out.println(" \n SHAWTY SAYS WHOOAAAAAA!!!!!!!!!!!!!!!");
 	}
 }
